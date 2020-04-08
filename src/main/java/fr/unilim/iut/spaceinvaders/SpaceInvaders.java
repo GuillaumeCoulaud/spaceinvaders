ecb4fa00ad995f54b84b0ef976fdc1975b1f1810
Guillaume COULAUD <guillaume.coulaud@etu.unilim.fr>
2020-04-08 11:18:21 +0200

    fin objectif 1

--- src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
+++ src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
@@ -62,6 +62,19 @@ public class SpaceInvaders {
 		private boolean estDansEspaceJeu(int x, int y) {
 			return ((x >= 0) && (x < longueur)) && ((y >= 0) && (y < hauteur));
 		}
+
+		public void deplacerVaisseauVersLaDroite() {
+			 if (vaisseau.abscisse()< (longueur-1)) vaisseau.seDeplacerVersLaDroite();
+		 }
+
+		public void deplacerVaisseauVersLaGauche() {
+			 if (vaisseau.abscisse()> (1)) vaisseau.seDeplacerVersLaGauche();
+			
+			
+		}
+		
+		
+		
     }
 
    
