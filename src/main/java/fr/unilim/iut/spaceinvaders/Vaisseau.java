833efe83699ed8b1ea085c13e943621622c4f375
Guillaume COULAUD <guillaume.coulaud@etu.unilim.fr>
2020-05-19 14:15:20 +0200

    fonctionnalité 4 nettoyage de la classe vaisseau

--- src/main/java/fr/unilim/iut/spaceinvaders/Vaisseau.java
+++ src/main/java/fr/unilim/iut/spaceinvaders/Vaisseau.java
@@ -3,25 +3,12 @@ package fr.unilim.iut.spaceinvaders;
 import fr.unilim.iut.spaceinvaders.utils.Dimension;
 import fr.unilim.iut.spaceinvaders.utils.Position;
 
-public class Vaisseau extends Sprite  {
+public class Vaisseau extends Sprite {
 
-    public Vaisseau(int longueur, int hauteur) {
-	    this(longueur, hauteur, 0, 0);
-    }
 
-   public Vaisseau(int longueur, int hauteur, int x, int y) {
-	   this(new Dimension(longueur, hauteur), new Position(x, y));
-    }
-
-    public Vaisseau(Dimension dimension, Position positionOrigine) {
-    	this(dimension, positionOrigine, 1);
-    }
     
-    public Vaisseau(Dimension dimension, Position positionOrigine, int vitesse) {
-		this.dimension = dimension;
-		this.origine = positionOrigine;
-		this.vitesse = vitesse;
+	
+	public Vaisseau(Dimension dimension, Position positionOrigine, int vitesse) {
+		super(dimension, positionOrigine, vitesse);
 	}
-
- 
 }
